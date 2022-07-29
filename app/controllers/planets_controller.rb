77da3723de8e77427aefce04867a23f13592b8a7
Adrian Greksa <adrian.greksa@gmail.com>
2022-07-29 10:02:39 +1000

    fixed parameters

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -38,37 +38,37 @@ class PlanetsController < ApplicationController
     if chance > 8
       @planet.name = RandomNameGenerator.new.compose(3) + '-' + RandomNameGenerator.new.compose(5) + '-' + rand(10).to_s
       @planet.fuel = rand(100000)
-      @planet.food = rand(10000)
+      @planet.food = rand(40000)
       @planet.gold = rand(1000000)
       rand(10).times.map {
         @moon = @planet.moons.new
         @moon.name = rand(10).to_s + ('a'..'z').to_a.sample
         @moon.fuel = rand(10000)
-        @moon.food = rand(4000)
+        @moon.food = rand(10000)
         @moon.gold = rand(100000)
       } 
     elsif chance > 6
       @planet.name = RandomNameGenerator.new.compose(2) + '-' + RandomNameGenerator.new.compose(2)
       @planet.fuel = rand(35000)
-      @planet.food = rand(10000)
-      @planet.gold = rand(35000)
+      @planet.food = rand(20000)
+      @planet.gold = rand(50000)
       rand(6).times.map {
         @moon = @planet.moons.new
         @moon.name = rand(10).to_s + ('a'..'z').to_a.sample
         @moon.fuel = rand(5000)
-        @moon.food = rand(2000)
+        @moon.food = rand(5000)
         @moon.gold = rand(10000)
       } 
     else
       @planet.name = RandomNameGenerator.new.compose(2)
       @planet.fuel = rand(10000)
-      @planet.food = rand(6000)
-      @planet.gold = rand(10000)
+      @planet.food = rand(10000)
+      @planet.gold = rand(20000)
       rand(3).times.map {
         @moon = @planet.moons.new
         @moon.name = rand(10).to_s + ('a'..'z').to_a.sample
         @moon.fuel = rand(5000)
-        @moon.food = rand(2000)
+        @moon.food = rand(5000)
         @moon.gold = rand(10000)
       } 
     end
